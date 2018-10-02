680d57338ddbccf5cab0b512c10eea2e1386b102
carloscubas <carlos.cubas@gmail.com>
2018-10-02 19:49:36 -0300

    commit inicial

---
+++ cap1/app/src/main/java/br/cubas/monitoramento/ModoAviao.kt
@@ -0,0 +1,13 @@
+package br.cubas.monitoramento
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ModoAviao : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented")
+    }
+
+}
