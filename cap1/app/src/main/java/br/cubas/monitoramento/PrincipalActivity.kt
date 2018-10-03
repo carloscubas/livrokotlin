ccee8e13142a71c4fda88c2762a4860a2ccaa2a1
carloscubas <carlos.cubas@gmail.com>
2018-10-03 20:51:51 -0300

    ajuste do app de que detecta se o modo avião está habilitado ou não

--- cap1/app/src/main/java/br/cubas/monitoramento/PrincipalActivity.kt
+++ cap1/app/src/main/java/br/cubas/monitoramento/PrincipalActivity.kt
@@ -1,52 +1,54 @@
 package br.cubas.monitoramento
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.IntentFilter
+import android.content.*
 import android.os.Bundle
 import android.provider.Settings
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.telephony.SmsMessage
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
-
-import kotlinx.android.synthetic.main.activity_principal.*
-import org.greenrobot.eventbus.EventBus
-import java.util.logging.Logger
-import org.greenrobot.eventbus.Subscribe
-
-
 
 class PrincipalActivity : AppCompatActivity() {
 
     var receiver: BroadcastReceiver? = null
+    var texto: TextView? = null
+    var imagem: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
-        EventBus.getDefault().register(this);
+        texto = findViewById<TextView>(R.id.texto)
+        imagem = findViewById<ImageView>(R.id.aviao)
+
         configureReceiver()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(receiver)
-        EventBus.getDefault().unregister(this)
-
     }
 
-    @Subscribe
-    fun onMessageEvent(event: Boolean) {
-        val changePlaneMode = event
-        Toast.makeText(this, "Broadcast : Flight mode changed.", Toast.LENGTH_LONG).show()
+    val modoAviao = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if(isAirplaneModeOn(contentResolver)){
+                texto?.text = "Modo avião ligado"
+                imagem?.setImageResource(R.drawable.flyon)
+            }else{
+                texto?.text = "Modo avião desligado"
+                imagem?.setImageResource(R.drawable.flyoff)
+            }
+        }
     }
 
     private fun configureReceiver() {
         val filter = IntentFilter()
-        filter.addAction("br.cubas.monitoramento.ModoAviao")
         filter.addAction("android.intent.action.AIRPLANE_MODE")
-        receiver = ModoAviao()
+        receiver = modoAviao
         registerReceiver(receiver, filter)
+    }
+
+    private fun isAirplaneModeOn(context: ContentResolver) : Boolean {
+        return Settings.System.getInt(context, Settings.Global.AIRPLANE_MODE_ON , 0) != 0
     }
 }
