afd293abb6c74a4c4ce5037e21dd9b9164867df5
s2it_csilva <carlos.cubas@gmail.com>
2018-10-03 14:29:00 -0300

    ajuste codigo background service

--- cap1/app/src/main/java/br/cubas/monitoramento/PrincipalActivity.kt
+++ cap1/app/src/main/java/br/cubas/monitoramento/PrincipalActivity.kt
@@ -8,9 +8,14 @@ import android.provider.Settings
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.telephony.SmsMessage
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_principal.*
+import org.greenrobot.eventbus.EventBus
 import java.util.logging.Logger
+import org.greenrobot.eventbus.Subscribe
+
+
 
 class PrincipalActivity : AppCompatActivity() {
 
@@ -19,12 +24,22 @@ class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        EventBus.getDefault().register(this);
         configureReceiver()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(receiver)
+        EventBus.getDefault().unregister(this)
+
+    }
+
+    @Subscribe
+    fun onMessageEvent(event: Boolean) {
+        val changePlaneMode = event
+        Toast.makeText(this, "Broadcast : Flight mode changed.", Toast.LENGTH_LONG).show()
     }
 
     private fun configureReceiver() {
